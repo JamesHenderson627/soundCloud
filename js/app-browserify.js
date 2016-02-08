4dc85df7e8506b5a50c88bbf694eceb0484f7bfd
JamesHenderson627 <jhenderson627@gmail.com>
2016-02-08 13:06:28 -0600

    updated README

--- js/app-browserify.js
+++ js/app-browserify.js
@@ -5,7 +5,8 @@ let fetch = require('./fetcher')
 
 var $ = require('jquery'),
 	Backbone = require('backbone'),
-	React = require('react')
+	React = require('react'),
+	SC = require('soundcloud')
 
 console.log('loaded javascript')
 // other stuff that we don't really use in our own code
@@ -21,114 +22,52 @@ console.log('loaded javascript')
     // new Router()
 // }
 
-//apiKey=ee61fbdb48ee35377e1847f4e2af152d
+var	clientId = "ee61fbdb48ee35377e1847f4e2af152d"
 //apiSecret=ee61fbdb48ee35377e1847f4e2af152d
+SC.initialize({
+	client_id: clientId
+})
 
 //====================COLLECTION============
-var TrackCollection = Backbone.Collection.extend({
-	url: "https://api.soundcloud.com/tracks?client_id=ee61fbdb48ee35377e1847f4e2af152d",
-
-	parse: function(repsonseData) {
-		// console.log("Response data")
-		// console.log(repsonseData)
-		return repsonseData
-	}
-})
-//=====================VIEW=================
-var TrackView = React.createClass({
-	componentWillMount: function(){
-		console.log("TrackView render imminent. Resistence is futile!")
-		this.props.tracksInfo.on("update sync", this.forceUpdate.bind(this))
-		console.log(this.props.tracksInfo)
-	}, 
-
-	_getSingleTrack: function(trackObj){ 
-		return(<SingleTrack single={trackObj} />)
-	},
-
-	render: function(){
-		var trackInfo = this.props.tracksInfo.models		
-			return(
-				<div id="overview">
-					<FixedBar />
-					{trackInfo.map(this._getSingleTrack).slice(0,10)}
-				</div>
-			)
-	}
-})
-
-var FixedBar = React.createClass({
-	render: function() {
-		return(
-			<div id="topBar">
-				<img id="logo" src="http://putyourjammieson.files.wordpress.com/2011/07/sound_wave_by_ex_astris1701-d41s0be.jpg" />
-				<h3>Welcome to RoundSound!!</h3>
-				<SearchBar />
-			</div>
-			)
-	}
-})
-
-var SearchBar = React.createClass({
-	render: function() {
-		return(
-			<div id="intro">
-				<input type="text" placeholder="Search"></input>
-			</div>
-			)
-	}
-})
-
-var SingleTrack = React.createClass({
-	componentDidMount: function() {
-		console.log("models")
+// var TrackCollection = Backbone.Collection.extend({
+// 	url: "https://api.soundcloud.com/playlists/180461791?cliend_id=" + clientId),
+// })
+//=====================VIEW==================
+var Playlist = React.createClass({
+	componentWillMount: function() {
 		console.log(this)
 	},
 
-	render: function(){
-		var stream = this.props.single.attributes.stream_url + "?client_id=ee61fbdb48ee35377e1847f4e2af152d",
-			userName = this.props.single.attributes.user.username,
-			pic = ""
-		
-		if (this.props.single.attributes.user.avatar_url) {
-			pic=this.props.single.attributes.user.avatar_url
-		} else {
-			pic = "http://img10.deviantart.net/ce46/i/2008/160/a/8/itunes_sheet_music_icon_by_purds.jpg"
-		}
+	render: function() {
 		return(
-			<div className="single">
-				<img className="profilePic" src={pic} />
-				<p className="userName">{userName}</p>
-				<audio className="song" src={stream} controls autoplay></audio>
+			<div>
+				<p>We are here!</p>
 			</div>
 			)
 	}
-
 })
-
 //==========================ROUTER===========
 var TrackRouter = Backbone.Router.extend({
 	routes: {
-		"*anyroute": "showDefaultTracks"
+		"*anyroute": "showDefault"
 	},
 
-	showDefaultTracks: function(){
-		var self = this
-		console.log("Getting the music!")
-		console.log(this.tc)
-		React.render(<TrackView tracksInfo={self.tc} />,document.querySelector("#container"))
-		this.tc.fetch({
-			dataType: "jsonp",
-			pracessData: true
+	showDefault: function() {
+		SC.get("/tracks", {
+			q:"metalocalypse",
+			license: "cc-by-sa"
+		}).then(function(tracks){
+			React.render(<Playlist playlistPage={tracks} />,document.querySelector("#container"))
 		})
 	},
 
-	initialize: function(){
-		this.tc = new TrackCollection()
-		console.log("I am a collection")
+	initialize: function() {
 		Backbone.history.start()
 	}
+
 })
 
 var tr = new TrackRouter()
+
+
 
